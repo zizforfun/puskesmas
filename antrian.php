d2242d76e856f026b19da194f34e93f520a0aeef
themekiman <sursurem@gmail.com>
2025-11-26 13:41:12 +0700

    oke

--- antrian.php
+++ antrian.php
@@ -36,6 +36,7 @@ while ($row = mysqli_fetch_assoc($result)) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cek Antrian</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css" rel="stylesheet">
     <link href="css/style.css" rel="stylesheet">
 
     <style>
